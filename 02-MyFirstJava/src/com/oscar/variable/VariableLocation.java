1caade3f81e9b458f6b0cb5f554a8509a9e5efee
LinGaOscar <oscar987123@yahoo.com.tw>
2022-01-12 17:18:36 +0800

    variable

---
+++ 02-MyFirstJava/src/com/oscar/variable/VariableLocation.java
@@ -0,0 +1,15 @@
+package com.oscar.variable;
+
+public class VariableLocation {
+	static int a =123;
+	public static void main(String[] args) {
+		int b =567;
+		// TODO Auto-generated method stub
+		checker();
+	}
+	
+	static void checker() {
+		System.out.println(a);
+		//System.out.println(b);
+	}
+}
---
+++ 02-MyFirstJava/src/com/oscar/variable/VariableReference.java
@@ -0,0 +1,27 @@
+package com.oscar.variable;
+
+public class VariableReference {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str1 = "Hello";
+		String str2 = "World";
+		String str3 = "HelloWorld";
+		String str4;
+
+		str4 = str1 + str2;
+		System.out.println(str3 == str4);
+
+		str4 = (str1 + str2).intern(); // find string in spring pool and return
+		System.out.println(str3 == str4);
+
+		String str11 = "HelloWorld";
+		String str12 = new String("HelloWorld");
+		String str13 = "HelloWorld";
+		System.out.println(str11 == str12); // different Reference in spring pool
+		System.out.println(str11 == str13); // same Reference in spring pool
+		System.out.println(str11.equals(str12)); // same value
+
+	}
+
+}
