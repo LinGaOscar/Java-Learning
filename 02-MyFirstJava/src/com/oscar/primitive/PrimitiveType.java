5fba30b39666090a0036b014a2db70797c50c5fb
LinGaOscar <oscar987123@yahoo.com.tw>
2022-01-12 15:37:19 +0800

    update

--- 02-MyFirstJava/src/com/oscar/primitive/PrimitiveType.java
+++ 02-MyFirstJava/src/com/oscar/primitive/PrimitiveType.java
@@ -3,7 +3,7 @@ package com.oscar.primitive;
 public class PrimitiveType {
 
 	public static void main(String[] args) {
-		// byte, short, int ,long
+		// byte, short, int ,long => 2^8, 2^16, 2^32, 2^64
 		int numInt = 123456; // -2147483648 to +2147483647
 		short numShort = 32767; // -32768 to +32767
 		long numLong = 888888888888L; // -9223372036854775808 to +9223372036854775807
@@ -27,6 +27,11 @@ public class PrimitiveType {
 		char var2 = '\u6145';
 		System.out.println("var1 : " + var1);
 		System.out.println("var2 : " + var2);
+
+		// String class type
+		char data[] = { 'a', 'b', 'c' };
+		String str = new String(data);
+		System.out.println(str);
 	}
 
 }
