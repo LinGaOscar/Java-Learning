2a1c793dd0320e9fa49812955864355fdbdb2f91
LinGaOscar <oscar987123@yahoo.com.tw>
2022-01-12 15:06:05 +0800

    build

--- 02-MyFirstJava/src/com/oscar/HelloWorld.java
+++ 02-MyFirstJava/src/com/oscar/HelloWorld.java
@@ -3,7 +3,8 @@ package com.oscar;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!!");
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
 	}
 
 }
