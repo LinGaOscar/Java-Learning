a0e9b7de66d11e8b87213b9cbdc0ba61f8e24a9a
LinGaOscar <oscar987123@yahoo.com.tw>
2022-01-12 17:43:17 +0800

    arrary

---
+++ 02-MyFirstJava/src/com/oscar/primitive/DataArray.java
@@ -0,0 +1,14 @@
+package com.oscar.primitive;
+
+public class DataArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr1 = { 1, 2, 3, 4, 5 }; // ¤@ºû°}¦C
+		System.out.println(arr1[1]);
+
+		int[][] arr2 = { { 37, 62 }, { 57, 94 }, { 45, 61 } }; // ¤G·L°}¦C
+		System.out.println(arr2[1][1]);
+	}
+
+}
