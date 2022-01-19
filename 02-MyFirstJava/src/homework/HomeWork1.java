dd29cc422e21919e13834fd5b91375cb9c471482
LinGaOscar <oscar987123@yahoo.com.tw>
2022-01-19 15:51:18 +0800

    update

--- 02-MyFirstJava/src/homework/HomeWork1.java
+++ 02-MyFirstJava/src/homework/HomeWork1.java
@@ -2,22 +2,23 @@ package homework;
 
 public class HomeWork1 {
 
-  public static void main(String[] args) {
-    for (int i = 1; i <= 100; i++) {
-      if (i % 10 == 1 && i > 10) {
-        System.out.println();
-      }
-      System.out.print(i + " \t");
-    }
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 10 == 1 && i > 10) {
+                System.out.println();
+            }
+            System.out.print(i + " \t");
+        }
 
-    //HW 9*9
-    // for (int i = 1; i < 10; i++) {
-    //   for (int j = 1; j < 10; j++) {
-    //     if (j % 10 == 1 && i != 1) {
-    //       System.out.println();
-    //     }
-    //     System.out.print(i * j + "\t");
-    //   }
-    // }
-  }
+
+        //HW 9*9
+        // for (int i = 1; i < 10; i++) {
+        //   for (int j = 1; j < 10; j++) {
+        //     if (j % 10 == 1 && i != 1) {
+        //       System.out.println();
+        //     }
+        //     System.out.print(i * j + "\t");
+        //   }
+        // }
+    }
 }
--- 02-MyFirstJava/src/homework/HomeWork2.java
+++ 02-MyFirstJava/src/homework/HomeWork2.java
@@ -4,12 +4,12 @@ public class HomeWork2 {
 
   public static void main(String[] args) {
     int line = 5;
-    // for (int i = 1; i <= line; i++) {
-    // 	for (int j = 0; j < i; j++) {
-    // 		System.out.print("*");
-    // 	}
-    // 	System.out.println();
-    // }
+     for (int i = 1; i <= line; i++) {
+     	for (int j = 0; j < i; j++) {
+     		System.out.print("*");
+     	}
+     	System.out.println();
+     }
 
     //HW another half
     // for (int i = 1; i <= line; i++) {
@@ -20,14 +20,14 @@ public class HomeWork2 {
     // }
 
     //pyramid
-    for (int i = 1; i <= line; i++) {
-      for (int n = 1; n <= line - i; n++) {
-        System.out.print(" ");
-      }
-      for (int j = 0; j < (2 * i - 1); j++) {
-        System.out.print("*");
-      }
-      System.out.println();
-    }
+//    for (int i = 1; i <= line; i++) {
+//      for (int n = 1; n <= line - i; n++) {
+//        System.out.print(" ");
+//      }
+//      for (int j = 0; j < (2 * i - 1); j++) {
+//        System.out.print("*");
+//      }
+//      System.out.println();
+//    }
   }
 }
